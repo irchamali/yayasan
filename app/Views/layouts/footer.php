86d816285c656c5d4ac7e6de253eafcdeb4356f8
irchamali <id.irchamali@gmail.com>
2025-02-21 19:33:10 +0700

    Update footer and search popup

--- app/Views/layouts/footer.php
+++ app/Views/layouts/footer.php
@@ -98,10 +98,10 @@
 	<div class="search-popup">
 		<div class="color-layer"></div>
 		<button class="close-search"><span class="flaticon-close-1"></span></button>
-		<form method="post" action="blog.html">
+		<form method="GET" action="/search">
 			<div class="form-group">
-				<input type="search" name="search-field" value="" placeholder="Search Here" required="">
-				<button class="fa fa-solid fa-magnifying-glass fa-fw" type="submit"></button>
+				<input type="search" name="search_query" value="" placeholder="Search Here" required="">
+				<button class="fa fa-solid fa-magnifying-glass fa-fw" type="submit" value="Search"></button>
 			</div>
 		</form>
 	</div>
